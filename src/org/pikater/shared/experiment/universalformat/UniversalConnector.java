522da23ea6608132e346ed989495cc987b17000e
Jiří Smolík <kukurka@gmail.com>
2014-05-02 00:05:12 +0200

    Merge branch 'master' of https://github.com/krajj7/pikater

---
+++ src/org/pikater/shared/experiment/universalformat/UniversalConnector.java
@@ -0,0 +1,30 @@
+package org.pikater.shared.experiment.universalformat;
+
+public class UniversalConnector {
+	
+    private UniversalElement universalDataProvider;
+    private String inputDataType;
+	private String outputDataType;
+
+    public String getInputDataType() {
+		return inputDataType;
+	}
+	public void setInputDataType(String inputDataType) {
+		this.inputDataType = inputDataType;
+	}
+
+	public String getOutputDataType() {
+		return outputDataType;
+	}
+	public void setOutputDataType(String dataType) {
+		this.outputDataType = dataType;
+	}
+
+	public UniversalElement getUniversalDataProvider() {
+		return universalDataProvider;
+	}
+	public void setUniversalDataProvider(UniversalElement universalDataProvider) {
+		this.universalDataProvider = universalDataProvider;
+	}
+
+}
---
+++ src/org/pikater/shared/utilities/pikaterDatabase/daos/ExperimentDAO.java
@@ -0,0 +1,55 @@
+package org.pikater.shared.utilities.pikaterDatabase.daos;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.pikater.shared.database.jpa.ExperimentStatus;
+import org.pikater.shared.database.jpa.JPABatch;
+import org.pikater.shared.database.jpa.JPAExperiment;
+import org.pikater.shared.utilities.pikaterDatabase.newDB.AbstractDAO;
+import org.pikater.shared.utilities.pikaterDatabase.newDB.EntityManagerInstancesCreator;
+
+public class ExperimentDAO extends AbstractDAO {
+
+	@Override
+	public String getEntityName() {
+		return (new JPAExperiment()).getEntityName();
+	}
+
+	@Override
+	public List<JPAExperiment> getAll() {
+		return EntityManagerInstancesCreator
+				.getEntityManagerInstance()
+				.createNamedQuery("Experiment.getAll", JPAExperiment.class)
+				.getResultList();
+	}
+
+	@Override
+	public List<JPAExperiment> getByID(int ID) {
+		return getByTypedNamedQuery("Experiment.getByID", "id", ID);
+	}
+	
+	public List<JPAExperiment> getByBatch(JPABatch batch) {
+		return getByTypedNamedQuery("Experiment.getByBatch", "batch", batch);
+	}
+	
+	public List<JPAExperiment> getByStatus(ExperimentStatus status) {
+		return getByTypedNamedQuery("Experiment.getByStatus", "status", status);
+	}
+	
+	private List<JPAExperiment> getByTypedNamedQuery(String queryName,String paramName,Object param){
+		EntityManager em=EntityManagerInstancesCreator.getEntityManagerInstance();
+		try{
+			return
+				em
+				.createNamedQuery(queryName,JPAExperiment.class)
+				.setParameter(paramName, param)
+				.getResultList();
+		}finally{
+			em.close();
+		}
+	}
+
+	
+}
