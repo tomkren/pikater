d9eb6af787d44398914e1d9135a08e724dcf238c
Jiří Smolík <kukurka@gmail.com>
2014-05-08 18:52:47 +0200

    First shot at converting from uni to web format.

--- src/org/pikater/shared/experiment/UniToWebFormatConverter.java
+++ src/org/pikater/shared/experiment/UniToWebFormatConverter.java
@@ -1,6 +1,12 @@
 package org.pikater.shared.experiment;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.pikater.shared.experiment.universalformat.UniversalComputationDescription;
+import org.pikater.shared.experiment.universalformat.UniversalConnector;
+import org.pikater.shared.experiment.universalformat.UniversalElement;
+import org.pikater.shared.experiment.webformat.BoxInfo;
 import org.pikater.shared.experiment.webformat.BoxInfoCollection;
 import org.pikater.shared.experiment.webformat.SchemaDataSource;
 
@@ -19,8 +25,47 @@ public class UniToWebFormatConverter
 		}
 		
 		// and then onto the conversion
-		
-		// TODO:
-		return null;
+		if(uniFormat.isGUICompatible())
+		{
+			SchemaDataSource webFormat = new SchemaDataSource();
+
+			// first convert all boxes
+			Map<UniversalElement, Integer> uniBoxToWebBoxID = new HashMap<UniversalElement, Integer>();
+			for(UniversalElement element : uniFormat.getElements())
+			{
+				BoxInfo info = boxInfoProvider.getByID(element.getElement().getType().getName()); 
+				if(info != null)
+				{
+					Integer convertedBoxID = webFormat.addLeafBoxAndReturnID(element.getGui(), info);
+					uniBoxToWebBoxID.put(element, convertedBoxID);
+				}
+				else
+				{
+					throw new ConversionException(new IllegalStateException(String.format(
+							"No box definitions for ontology '%s'.", element.getElement().getType().getName())));
+				}
+			}
+			
+			// then convert all edges
+			for(UniversalElement element : uniFormat.getElements())
+			{
+				for(UniversalConnector edge : element.getElement().getInputSlots())
+				{
+					webFormat.connect(
+							uniBoxToWebBoxID.get(edge.getUniversalDataProvider()),
+							uniBoxToWebBoxID.get(element)
+					);
+				}
+			}
+			
+			// TODO: wrapper boxes, options & stuff
+			
+			return webFormat;
+		}
+		else
+		{
+			throw new ConversionException(new IllegalArgumentException(String.format(
+					"The universal format below is not fully compatible with the GUI (web) format.\n%s", uniFormat.exportXML())));
+		}
 	}
 }
