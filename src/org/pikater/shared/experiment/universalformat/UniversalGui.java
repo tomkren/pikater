33f70626ccc3ebf86df47d76821eb652cedaec8a
Jiří Smolík <kukurka@gmail.com>
2014-05-24 00:51:28 +0200

    Adding a specific ctor to be used from the client.

--- src/org/pikater/shared/experiment/universalformat/UniversalGui.java
+++ src/org/pikater/shared/experiment/universalformat/UniversalGui.java
@@ -2,6 +2,8 @@ package org.pikater.shared.experiment.universalformat;
 
 import java.io.Serializable;
 
+import net.edzard.kinetic.Vector2d;
+
 public class UniversalGui implements Serializable
 {
 	private static final long serialVersionUID = 602561730450272920L;
@@ -15,10 +17,16 @@ public class UniversalGui implements Serializable
 	protected UniversalGui()
 	{
 	}
-
+	
 	public UniversalGui(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
+	}
+
+	public UniversalGui(Vector2d absoluteBoxPosition)
+	{
+		this.x = (int) absoluteBoxPosition.x;
+		this.y = (int) absoluteBoxPosition.y;
 	}
 }
