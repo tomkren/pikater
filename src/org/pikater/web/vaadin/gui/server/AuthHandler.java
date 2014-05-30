9de16943d44ce556dcfb59bcb389243cd0067ca1
Jiří Smolík <kukurka@gmail.com>
2014-05-30 10:59:14 +0200

    All authentication code is here for simplicity.

---
+++ src/org/pikater/web/vaadin/gui/server/AuthHandler.java
@@ -0,0 +1,101 @@
+package org.pikater.web.vaadin.gui.server;
+
+import java.util.List;
+
+import org.pikater.shared.database.jpa.JPAUser;
+import org.pikater.shared.database.jpa.daos.DAOs;
+import org.pikater.web.config.ServerConfigurationInterface;
+
+import com.vaadin.server.VaadinSession;
+
+public class AuthHandler
+{
+	private static final String sessionKey_userID = "sk_uid";
+	
+	/**
+	 * Tries to authenticate the user. If the provided auth info is correct, automatically
+	 * authenticates the user in this UI.
+	 * @param session
+	 * @param login
+	 * @param password
+	 * @return true if the provided auth info was correct
+	 */
+	public static boolean authenticateUser(VaadinSession session, String login, String password)
+	{
+		if(ServerConfigurationInterface.avoidUsingDBForNow())
+		{
+			return true;
+		}
+		else
+		{
+			List<JPAUser> usersWithProvidedLogin = DAOs.userDAO.getByLogin(login);
+			for(JPAUser user : usersWithProvidedLogin)
+			{
+				if(user.getPassword().equals(password)) // password match
+				{
+					login(session, user.getId()); // actually authenticate in this UI
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+	
+	public static Integer getUserID(VaadinSession session)
+	{
+		if(session == null)
+		{
+			throw new NullPointerException("Session is null. Did you use the 'getSession()' method from a constructor? "
+					+ "Don't forget that session is available only since the method 'attach' is called on your component.");
+		}
+		else
+		{
+			return (Integer) session.getAttribute(sessionKey_userID);
+		}
+	}
+	
+	public static JPAUser getUserEntity(VaadinSession session)
+	{
+		return DAOs.userDAO.getByIDWithException(getUserID(session));
+	}
+	
+	public static boolean isUserAuthenticated(VaadinSession session)
+	{
+		return getUserID(session) != null;
+	}
+	
+	private static void login(VaadinSession session, int userID)
+	{
+		if(isUserAuthenticated(session))
+		{
+			throw new IllegalStateException("User is already authenticated in this session.");
+		}
+		else
+		{
+			session.setAttribute(sessionKey_userID, userID);
+			NoSessionStore.bind(userID, session);
+		}
+	}
+	
+	public static void logout(VaadinSession session)
+	{
+		if(isUserAuthenticated(session))
+		{
+			NoSessionStore.unbind(getUserID(session), session);
+			session.setAttribute(sessionKey_userID, null);
+			
+			// Redirect from the page
+			// getUI().getPage().setLocation("/myapp/logoutpage.html");
+			
+			/*
+			 * Don't close the whole session to avoid redirecting to a logout page. Instead,
+			 * close all non-main UIs manually and reset the content 
+			 */
+			// getSession().getUIs()
+		}
+		else
+		{
+			throw new IllegalStateException("User is not authenticated.");
+		}
+	}
+}
