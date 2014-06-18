5b0d91c3a414943765dcb4fc03ea72e82e393ee8
Jiří Smolík <kukurka@gmail.com>
2014-06-19 01:01:06 +0200

    Updating previously implemented view to use it

--- src/org/pikater/web/vaadin/gui/server/ui_default/indexpage/content/admin/UsersView.java
+++ src/org/pikater/web/vaadin/gui/server/ui_default/indexpage/content/admin/UsersView.java
@@ -2,26 +2,22 @@ package org.pikater.web.vaadin.gui.server.ui_default.indexpage.content.admin;
 
 import org.pikater.shared.database.views.jirka.users.UsersTableView;
 import org.pikater.web.vaadin.gui.server.ui_default.indexpage.content.ContentProvider.IContentComponent;
-import org.pikater.web.vaadin.tabledbview.TableDBContainer;
+import org.pikater.web.vaadin.tabledbview.DBTableLayout;
 
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.VerticalLayout;
 
-public class UsersView extends VerticalLayout implements IContentComponent
+public class UsersView extends DBTableLayout implements IContentComponent
 {
 	private static final long serialVersionUID = -6440037882833424701L;
 	
 	public UsersView()
 	{
-		super();
-		setSizeFull();
+		super(new UsersTableView(), UsersTableView.Column.LOGIN);
+		setSizeUndefined();
 		
-		Table table_users = new Table();
-		table_users.setSizeFull();
-		table_users.setContainerDataSource(new TableDBContainer(table_users, new UsersTableView()));
-		
-		addComponent(table_users);
+		// TODO: set column sizes
+		// this.table.setColumnWidth(column, -1);
+		// this.table.setColumnExpandRatio(propertyId, expandRatio);
 	}
 
 	@Override
