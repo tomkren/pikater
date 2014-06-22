dfc30a0627d353e4b9a08cfd747891fc7f2545f4
Klara Peskova <ustav@seznam.cz>
2014-06-22 13:59:09 +0200

    search strategy

---
+++ core/inputs/inputsKlara/input1/xmlGenerator/Input1.java
@@ -0,0 +1,81 @@
+package xmlGenerator;
+
+import org.pikater.core.agents.experiment.computing.Agent_WekaRBFNetworkCA;
+import org.pikater.core.agents.system.Agent_GUIKlara;
+import org.pikater.core.dataStructures.options.Converter;
+import org.pikater.core.dataStructures.options.StepanuvOption;
+import org.pikater.core.dataStructures.options.types.OptionValue;
+import org.pikater.core.ontology.subtrees.batchDescription.ComputationDescription;
+import org.pikater.core.ontology.subtrees.batchDescription.ComputingAgent;
+import org.pikater.core.ontology.subtrees.batchDescription.DataSourceDescription;
+import org.pikater.core.ontology.subtrees.batchDescription.FileDataSaver;
+import org.pikater.core.ontology.subtrees.task.EvaluationMethod;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+//Example: Single datasource -> single computing agent. -> Single save
+public final class Input1 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		System.out.println("Exporting Ontology input1 to Klara's input XML configuration file.");
+
+        //Specify a datasource
+        DataSourceDescription fileDataSource=new DataSourceDescription("weather.arff");
+
+        //Create validation method for a computing agent
+        EvaluationMethod evaluationMethod = new EvaluationMethod();
+        evaluationMethod.setName("CrossValidation");
+        
+        //Create cross validation option
+        StepanuvOption optionF = new StepanuvOption();
+        optionF.setName("F");
+        optionF.setOption( new OptionValue(8) );
+                
+        evaluationMethod.addOption( Converter.toOption(optionF) );
+        
+        
+        //Create two options for single computing agent
+        StepanuvOption optionS = new StepanuvOption();
+        optionS.setName("S");
+        optionS.setOption( new OptionValue(1) );
+
+        StepanuvOption optionM = new StepanuvOption();
+        optionM.setName("M");
+        optionM.setOption( new OptionValue(-2) );
+
+        //Create new computing agent, add options and datasource that we have created above
+		ComputingAgent comAgent = new ComputingAgent();
+		comAgent.setAgentType(Agent_WekaRBFNetworkCA.class.getName());
+		comAgent.addOption( Converter.toOption(optionS) );
+		comAgent.addOption( Converter.toOption(optionM) );
+		comAgent.setTrainingData(fileDataSource);
+		comAgent.setTestingData(fileDataSource);
+		comAgent.setEvaluationMethod(evaluationMethod);
+
+        //Labeled data labeled by our CA are the new datasource
+		DataSourceDescription computingDataSource = new DataSourceDescription();
+		computingDataSource.setDataType("Data");
+		computingDataSource.setDataProvider(comAgent);
+
+        //Save labeled data
+        FileDataSaver saver = new FileDataSaver();
+        saver.setDataSource(computingDataSource);
+
+        //Our requirements for the description are ready, lets create new computation description
+        List<FileDataSaver> roots = new ArrayList<>();
+        roots.add(saver);
+        
+        ComputationDescription comDescription = new ComputationDescription();
+        comDescription.setRootElements(roots);
+
+		String fileName = Agent_GUIKlara.filePath + "input1"
+				+ System.getProperty("file.separator")
+				+ "input1.xml";
+
+		comDescription.exportXML(fileName);
+    }
+}
