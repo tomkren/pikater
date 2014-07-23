e75723e1338d10afa5b6521c0243ba22dcdac696
Péter Sipos <siposp@gmail.com>
2014-07-23 19:21:56 +0200

    Visualisation Quartz Jobs Part II

--- src/org/pikater/shared/visualisation/TestVisual.java
+++ src/org/pikater/shared/visualisation/TestVisual.java
@@ -10,6 +10,7 @@ import org.pikater.shared.database.utils.ResultFormatter;
 import org.pikater.shared.visualisation.charts.axis.exception.AxisNotJoinableException;
 import org.pikater.shared.visualisation.charts.coloring.exception.ColorerNotMergeableException;
 import org.pikater.shared.visualisation.generator.ChartGenerator;
+import org.pikater.shared.visualisation.generator.quartz.ComparisonSVGGenerator;
 import org.pikater.shared.visualisation.generator.quartz.MatrixPNGGenerator;
 import org.pikater.shared.visualisation.generator.quartz.SingleSVGGenerator;
 
@@ -17,49 +18,34 @@ public class TestVisual {
 
 	public static void main(String[] args) throws IOException, AxisNotJoinableException, ColorerNotMergeableException {
 		long time=0;
-		JPADataSetLO iris=new ResultFormatter<JPADataSetLO>(DAOs.dataSetDAO.getByDescription("iris")).getSingleResultWithNull();
+		JPADataSetLO iris1=new ResultFormatter<JPADataSetLO>(DAOs.dataSetDAO.getByDescription("iris")).getSingleResultWithNull();
+		JPADataSetLO iris2=new ResultFormatter<JPADataSetLO>(DAOs.dataSetDAO.getByDescription("iris2")).getSingleResultWithNull();
 		
 		String attr1="sepallength";
 		String attr2="petallength";
 		String attr3="class";
 		
-		System.out.println("Generating SVG Comparison Chart for: " +iris.getDescription()+" and "+iris.getDescription());
+		System.out.println("Generating SVG Comparison Chart for: " +iris1.getDescription()+" and "+iris2.getDescription());
 		time=System.currentTimeMillis();
-		ChartGenerator.generateSVGComparisonDatasetChart(iris, iris,new PrintStream("core/datasets/visual/sIRIS_sepallength_petallength_class_c.svg"), attr1, attr1, attr2, attr2,attr3, attr3);
-		System.out.println("Finished in: "+(System.currentTimeMillis()-time)+" ms");
-		/**
-		System.out.println("Generating SVG Single Chart for: " +iris.getDescription());
-		time=System.currentTimeMillis();
-		ChartGenerator.generateSVGSingleDatasetChart(
-				iris,
-				new PrintStream("core/datasets/visual/sIRIS"+attr1+"_"+attr2+"_"+attr3+"s.svg"), 
-				attr1, 
-				attr2,
-				attr3,
-				new DummyProgress());
+		//ChartGenerator.generateSVGComparisonDatasetChart(iris, iris,new PrintStream("core/datasets/visual/sIRIS_sepallength_petallength_class_c.svg"), attr1, attr1, attr2, attr2,attr3, attr3);
+		ComparisonSVGGenerator csvggiris=new ComparisonSVGGenerator(new DummyProgress(), new PrintStream("core/datasets/visual/sIRIS_sepallength_petallength_class_c.svg"), iris1, iris2, attr1, attr1, attr2, attr2, attr3, attr3);
+		csvggiris.create();
 		System.out.println("Finished in: "+(System.currentTimeMillis()-time)+" ms");
 		
-		**/
 		
 		List<JPADataSetLO> datasets = DAOs.dataSetDAO.getAll();
 		
 		for(JPADataSetLO dataset : datasets){
-			/**
+			
 			System.out.println("Generating SVG Comparison Chart for: " +dataset.getDescription()+" and "+dataset.getDescription());
 			time=System.currentTimeMillis();
-			ChartGenerator.generateSVGComparisonDatasetChart(dataset, dataset,new PrintStream("core/datasets/visual/"+dataset.getDescription()+"_0_1_2_comp.svg"), 0, 0, 1, 1, 2, 2);
+			ComparisonSVGGenerator csvgg=new ComparisonSVGGenerator(new DummyProgress(),new PrintStream("core/datasets/visual/"+dataset.getDescription()+"_0_1_2_comp.svg"),dataset, dataset, 0, 0, 1, 1, 2, 2);
+			csvgg.create();
 			System.out.println("Finished in: "+(System.currentTimeMillis()-time)+" ms");
-			**/
+			
 			System.out.println("Generating SVG Single Chart for: " +dataset.getDescription());
 			time=System.currentTimeMillis();
-			/**
-			ChartGenerator.generateSVGSingleDatasetChart(
-					dataset,
-					new PrintStream("core/datasets/visual/"+dataset.getDescription()+"_0_1_2s.svg"), 
-					0, 
-					1,
-					2,
-					new DummyProgress());**/
+			
 			SingleSVGGenerator ssvgg=new SingleSVGGenerator(new DummyProgress(), dataset,new PrintStream("core/datasets/visual/"+dataset.getDescription()+"_0_1_2s.svg"), 0, 1, 2);
 			ssvgg.create();
 			
@@ -67,18 +53,11 @@ public class TestVisual {
 			
 			System.out.println("Generating PNG Matrix Chart for: " +dataset.getDescription());
 			time=System.currentTimeMillis();
-			//ChartGenerator.generatePNGMatrixDatasetChart(dataset, new PrintStream("core/datasets/visual/"+dataset.getDescription()+".png"));
 			MatrixPNGGenerator mpngg=new MatrixPNGGenerator(new DummyProgress(), dataset, new PrintStream("core/datasets/visual/"+dataset.getDescription()+".png"));
 			mpngg.create();
 			
 			System.out.println("Finished in: "+(System.currentTimeMillis()-time)+" ms");
 			
-			/**
-			System.out.println("Generating SVG Matrix Chart for: " +dataset.getDescription());
-			time=System.currentTimeMillis();
-			ChartGenerator.generateSVGMatrixDatasetChart(dataset, new PrintStream("core/datasets/visual/"+dataset.getDescription()+".svg"));
-			System.out.println("Finished in: "+(System.currentTimeMillis()-time)+" ms");
-			**/
 		}
 	}
 }
---
+++ src/org/pikater/web/quartzjobs/SingleSVGGeneratorJob.java
@@ -0,0 +1,76 @@
+package org.pikater.web.quartzjobs;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.pikater.shared.database.jpa.JPADataSetLO;
+import org.pikater.shared.quartz.jobs.base.InterruptibleOneTimeJob;
+import org.pikater.shared.visualisation.generator.ChartGenerator.IProgressListener;
+import org.pikater.shared.visualisation.generator.quartz.SingleSVGGenerator;
+import org.quartz.JobBuilder;
+import org.quartz.JobExecutionException;
+import org.quartz.UnableToInterruptJobException;
+
+public class SingleSVGGeneratorJob extends InterruptibleOneTimeJob {
+
+	public SingleSVGGeneratorJob() {
+		super(6);
+	}
+
+	@Override
+	public void interrupt() throws UnableToInterruptJobException {
+	}
+
+	@Override
+	public boolean argumentCorrect(int index, Object arg) {
+		switch(index){
+		case 0:
+			return arg instanceof IProgressListener;
+		case 1:
+			return arg instanceof JPADataSetLO;
+		case 2:
+			return arg instanceof String;
+		case 3:
+		case 4:
+		case 5:
+			return ((arg instanceof Integer)||(arg instanceof String));
+		default:
+			return false;
+		}
+	}
+
+	@Override
+	public void buildJob(JobBuilder builder) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	protected void execute() throws JobExecutionException {
+			IProgressListener listener=getArg(0);
+			JPADataSetLO dslo=getArg(1);
+			try {
+				PrintStream output=new PrintStream((String)getArg(2));
+				Object attrArg1=getArg(3);
+				Object attrArg2=getArg(4);
+				Object attrArg3=getArg(5);
+				
+				SingleSVGGenerator ssvgg;
+				if((attrArg1 instanceof String)&&(attrArg2 instanceof String)&&(attrArg3 instanceof String)){
+					ssvgg=new SingleSVGGenerator(listener, dslo, output, (String)attrArg1,(String)attrArg2, (String)attrArg3);
+				}else if((attrArg1 instanceof Integer)&&(attrArg2 instanceof Integer)&&(attrArg3 instanceof Integer)){
+					ssvgg=new SingleSVGGenerator(listener, dslo, output, (Integer)attrArg1,(Integer)attrArg2, (Integer)attrArg3);
+				}else{
+					output.close();
+					throw new JobExecutionException();
+				}
+				
+				ssvgg.create();
+				
+			} catch (IOException e) {
+				throw new JobExecutionException();
+			}
+
+
+	}
+
+}
