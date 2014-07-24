26eb808ab9c9493f87ddae3225c9c45443dfe0dc
Stepan <bc.stepan.balcar@gmail.com>
2014-07-24 09:30:11 +0200

    CrossvalidationBox update

--- src/org/pikater/core/options/CrossvalidationEvaluationMethod_VirtualBox.java
+++ src/org/pikater/core/options/CrossvalidationEvaluationMethod_VirtualBox.java
@@ -2,17 +2,27 @@ package org.pikater.core.options;
 
 import org.pikater.core.ontology.subtrees.agentInfo.AgentInfo;
 import org.pikater.core.ontology.subtrees.batchDescription.EvaluationMethod;
+import org.pikater.core.ontology.subtrees.newOption.base.NewOption;
+import org.pikater.core.ontology.subtrees.newOption.restrictions.RangeRestriction;
+import org.pikater.core.ontology.subtrees.newOption.values.IntegerValue;
 
 public class CrossvalidationEvaluationMethod_VirtualBox {
 
 	public static AgentInfo get() {
 		
+		NewOption optionF = new NewOption("F", new IntegerValue(10), new RangeRestriction(
+				new IntegerValue(1),
+				new IntegerValue(20))
+		);
+		optionF.setDescription("F");
+
 		AgentInfo agentInfo = new AgentInfo();
 		agentInfo.setName("Crossvalidation Evaluation Method");
 		agentInfo.setDescription("Evaluation Method");
+		agentInfo.addOption(optionF);
 		agentInfo.importAgentClass(CrossvalidationEvaluationMethod_VirtualBox.class);
 		agentInfo.importOntologyClass(EvaluationMethod.class);
-		
+				
 		agentInfo.setOutputSlots(
 				AAA_SlotHelper.getCEvaluationMethodOutputSlots());
 
