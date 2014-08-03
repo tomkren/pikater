b56c6ab576442ec234c46df7bfdcd285b032dc48
Jiří Smolík <kukurka@gmail.com>
2014-08-03 22:26:57 +0200

    CustomOrderSet is now more usable (+ improvements)

--- src/org/pikater/shared/util/collections/CustomOrderSet.java
+++ src/org/pikater/shared/util/collections/CustomOrderSet.java
@@ -9,7 +9,26 @@ public class CustomOrderSet<T> extends TreeSet<T>
 	private static final long serialVersionUID = -7363544833191937694L;
 	
 	/**
-	 * Natural ordering instead of custom one.
+	 * Creates an empty set sorting with natural order (the element type has
+	 * to implement {@link Comparable}).
+	 */
+	public CustomOrderSet()
+	{
+		super();
+	}
+	
+	/**
+	 * Creates an empty set sorting with the given comparator.
+	 * @param comp
+	 */
+	public CustomOrderSet(Comparator<T> comp)
+	{
+		super(comp);
+	}
+
+	/**
+	 * Creates a duplicate of the given collection that sorts using natural
+	 * ordering (the element type has to implement {@link Comparable}). 
 	 * @param unsortedCollection
 	 */
 	public CustomOrderSet(Collection<T> unsortedCollection)
@@ -18,7 +37,7 @@ public class CustomOrderSet<T> extends TreeSet<T>
 	}
 
 	/**
-	 * Custom ordering.
+	 * Creates a duplicate of the given collection that sorts using the given comparator.
 	 * @param unsortedCollection
 	 * @param comp
 	 */
