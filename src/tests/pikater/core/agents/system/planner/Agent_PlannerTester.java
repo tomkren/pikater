30398cf87be0a13153de1e23c84b2317059f5e0d
krajj7 <krajj7@users.noreply.github.com>
2014-08-09 13:24:01 +0200

    revert "cleaning" nonsense
    This reverts commit c4c2b0d7af1d8917410a5e7aa329532e811b4d71.

--- src/tests/pikater/core/agents/system/planner/Agent_PlannerTester.java
+++ src/tests/pikater/core/agents/system/planner/Agent_PlannerTester.java
@@ -183,10 +183,8 @@ public class Agent_PlannerTester extends PikaterAgent {
             else
                 log("Reply received: "+ACLMessage.getPerformative(reply.getPerformative())+" "+reply.getContent());
 */
-        } catch (CodecException e) {
-            logError("Codec error occurred: "+e.getMessage(), e);
-        } catch (OntologyException e) {
-            logError("Ontology error occurred: "+e.getMessage(), e);
+        } catch (CodecException | OntologyException e) {
+            logError("Ontology/codec error occurred: "+e.getMessage(), e);
         }
 
     }
@@ -247,10 +245,8 @@ public class Agent_PlannerTester extends PikaterAgent {
             else
                 log("Reply received: "+ACLMessage.getPerformative(reply.getPerformative())+" "+reply.getContent());
 */
-        } catch (CodecException e) {
-            logError("Codec error occurred: "+e.getMessage(), e);
-        } catch (OntologyException e) {
-            logError("Ontology error occurred: "+e.getMessage(), e);
+        } catch (CodecException | OntologyException e) {
+            logError("Ontology/codec error occurred: "+e.getMessage(), e);
         }
 
     }
