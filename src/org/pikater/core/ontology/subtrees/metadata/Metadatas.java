ddbd2d677c29691f267601dfc6b5471b8ac21eee
Péter Sipos <siposp@gmail.com>
2014-08-12 06:05:47 +0200

    Fix NPE if no results are found

--- src/org/pikater/core/ontology/subtrees/metadata/Metadatas.java
+++ src/org/pikater/core/ontology/subtrees/metadata/Metadatas.java
@@ -1,5 +1,6 @@
 package org.pikater.core.ontology.subtrees.metadata;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import jade.content.Concept;
@@ -12,6 +13,10 @@ public class Metadatas implements Concept {
 	private static final long serialVersionUID = -7879865268316684239L;
 	
 	private List<Metadata> metadatas;
+	
+	public Metadatas(){
+		this.metadatas=new ArrayList<Metadata>();
+	}
 
 	public List<Metadata> getMetadatas() {
 		return metadatas;
