1024dfec10403837f1d5577fce952d82d3feef6b
Jiří Smolík <kukurka@gmail.com>
2014-08-16 20:50:02 +0200

    Little tweak of custom popups

--- src/org/pikater/web/vaadin/gui/server/components/popups/MyPopup.java
+++ src/org/pikater/web/vaadin/gui/server/components/popups/MyPopup.java
@@ -1,10 +1,12 @@
 package org.pikater.web.vaadin.gui.server.components.popups;
 
+import com.vaadin.annotations.StyleSheet;
 import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Window;
 
+@StyleSheet("myPopup.css")
 public class MyPopup extends Window
 {
 	private static final long serialVersionUID = 332265534464591922L;
@@ -26,6 +28,11 @@ public class MyPopup extends Window
 		this(caption);
 		
 		setContent(content);
+	}
+	
+	public void setCaptionCentered()
+	{
+		addStyleName("centeredCaption");
 	}
 	
 	public void show()
