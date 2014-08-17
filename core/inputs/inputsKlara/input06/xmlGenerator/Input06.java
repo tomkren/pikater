2e766de9157ce08c01f74df2b01337573d996db7
Stepan <bc.stepan.balcar@gmail.com>
2014-08-17 19:08:44 +0200

    Input 6,7 format

--- core/inputs/inputsKlara/input06/xmlGenerator/Input06.java
+++ core/inputs/inputsKlara/input06/xmlGenerator/Input06.java
@@ -11,39 +11,36 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 //Example: Single datasource -> single computing agent. -> Single save
 public final class Input06 {
 
 	public static ComputationDescription createDescription() {
-		
-        FileDataProvider fileDataProvider = new FileDataProvider();
-        fileDataProvider.setFileURI("weather.arff");
-        
-        //Specify a datasource
-        DataSourceDescription fileDataSource = new DataSourceDescription();
-        fileDataSource.setDataProvider(fileDataProvider);
 
-        //Create validation method for a computing agent
-        EvaluationMethod evaluationMethod =
-        		new EvaluationMethod(CrossValidation.class.getName());
-        
-        //Create cross validation option                
-        NewOption optionF = new NewOption("F",8);
-        evaluationMethod.addOption(optionF);        
-        
-        //Create two options for single computing agent
-        NewOption optionS = new NewOption("S",1);
-        NewOption optionM = new NewOption("M",2);
+		FileDataProvider fileDataProvider = new FileDataProvider();
+		fileDataProvider.setFileURI("weather.arff");
 
-        
-        
-        //Create data-processing
-        List<DataSourceDescription> dataSources = new ArrayList<DataSourceDescription>();
-        dataSources.add(fileDataSource);
-        dataSources.add(fileDataSource);
-        
-        DataProcessing dp = new DataProcessing();
+		// Specify a datasource
+		DataSourceDescription fileDataSource = new DataSourceDescription();
+		fileDataSource.setDataProvider(fileDataProvider);
+
+		// Create validation method for a computing agent
+		EvaluationMethod evaluationMethod = new EvaluationMethod(
+				CrossValidation.class.getName());
+
+		// Create cross validation option
+		NewOption optionF = new NewOption("F", 8);
+		evaluationMethod.addOption(optionF);
+
+		// Create two options for single computing agent
+		NewOption optionS = new NewOption("S", 1);
+		NewOption optionM = new NewOption("M", 2);
+
+		// Create data-processing
+		List<DataSourceDescription> dataSources = new ArrayList<DataSourceDescription>();
+		dataSources.add(fileDataSource);
+		dataSources.add(fileDataSource);
+
+		DataProcessing dp = new DataProcessing();
 		dp.setDataSources(dataSources);
 		dp.setAgentType(Agent_WeatherSplitter.class.getName());
 
@@ -51,8 +48,9 @@ public final class Input06 {
 		DataSourceDescription computingDataSource = new DataSourceDescription();
 		computingDataSource.setOutputType("Data");
 		computingDataSource.setDataProvider(dp);
-		
-        //Create new computing agent, add options and datasource that we have created above
+
+		// Create new computing agent, add options and datasource that we have
+		// created above
 		ComputingAgent comAgent = new ComputingAgent();
 		comAgent.setAgentType(Agent_WekaRBFNetworkCA.class.getName());
 		comAgent.addOption(optionS);
@@ -60,35 +58,35 @@ public final class Input06 {
 		comAgent.setTrainingData(computingDataSource);
 		comAgent.setTestingData(computingDataSource);
 		comAgent.setEvaluationMethod(evaluationMethod);
-		
+
 		DataSourceDescription computingAgentDataSource = new DataSourceDescription();
 		computingAgentDataSource.setOutputType("DataCA");
 		computingAgentDataSource.setDataProvider(comAgent);
 
-        //Save labeled data
-        FileDataSaver saver = new FileDataSaver();
-        saver.setDataSource(computingAgentDataSource);
+		// Save labeled data
+		FileDataSaver saver = new FileDataSaver();
+		saver.setDataSource(computingAgentDataSource);
 
-        //Our requirements for the description are ready, lets create new computation description
-        List<FileDataSaver> roots = new ArrayList<FileDataSaver>();
-        roots.add(saver);
-        
-        ComputationDescription comDescription = new ComputationDescription();
-        comDescription.setRootElements(roots);
+		// Our requirements for the description are ready, lets create new
+		// computation description
+		List<FileDataSaver> roots = new ArrayList<FileDataSaver>();
+		roots.add(saver);
 
-        return comDescription;
+		ComputationDescription comDescription = new ComputationDescription();
+		comDescription.setRootElements(roots);
+
+		return comDescription;
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		System.out.println("Exporting Ontology input06 to Klara's input XML configuration file.");
 
 		ComputationDescription comDescription = createDescription();
-		
+
 		String fileName = CoreConfiguration.INPUTS_KLARA_PATH + "input06"
-				+ System.getProperty("file.separator")
-				+ "input.xml";
+				+ System.getProperty("file.separator") + "input.xml";
 
 		comDescription.exportXML(fileName);
-    }
+	}
 }
--- core/inputs/inputsKlara/input07/xmlGenerator/Input07.java
+++ core/inputs/inputsKlara/input07/xmlGenerator/Input07.java
@@ -7,49 +7,47 @@ import org.pikater.core.ontology.subtrees.batchDescription.evaluationMethod.Cros
 import org.pikater.core.ontology.subtrees.newOption.base.NewOption;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-
 
 //Example: Single datasource -> single computing agent. -> Single save
 public final class Input07 {
 
 	public static ComputationDescription createDescription() {
-		
-        FileDataProvider fileDataProvider = new FileDataProvider();
-        fileDataProvider.setFileURI("weather.arff");
-        
-        //Specify a datasource
-        DataSourceDescription fileDataSource = new DataSourceDescription();
-        fileDataSource.setDataProvider(fileDataProvider);
 
-        //Create validation method for a computing agent
-        EvaluationMethod evaluationMethod =
-        		new EvaluationMethod(CrossValidation.class.getName());
-        
-        //Create cross validation option                
-        NewOption optionF = new NewOption("F",8);
-        
-        evaluationMethod.addOption(optionF);
-        
-        
-        //Create two options for single computing agent
-        NewOption optionS = new NewOption("S",1);
+		FileDataProvider fileDataProvider = new FileDataProvider();
+		fileDataProvider.setFileURI("weather.arff");
 
-        NewOption optionM = new NewOption("M",2);
+		// Specify a datasource
+		DataSourceDescription fileDataSource = new DataSourceDescription();
+		fileDataSource.setDataProvider(fileDataProvider);
 
-        //Create new computing agent, add options and datasource that we have created above
+		// Create validation method for a computing agent
+		EvaluationMethod evaluationMethod = new EvaluationMethod(
+				CrossValidation.class.getName());
+
+		// Create cross validation option
+		NewOption optionF = new NewOption("F", 8);
+
+		evaluationMethod.addOption(optionF);
+
+		// Create two options for single computing agent
+		NewOption optionS = new NewOption("S", 1);
+
+		NewOption optionM = new NewOption("M", 2);
+
+		// Create new computing agent, add options and datasource that we have
+		// created above
 		ComputingAgent comAgent1 = new ComputingAgent();
 		comAgent1.setAgentType(Agent_WekaRBFNetworkCA.class.getName());
 		comAgent1.addOption(optionS);
 		comAgent1.addOption(optionM);
 		comAgent1.setTrainingData(fileDataSource);
 		comAgent1.setTestingData(fileDataSource);
-		comAgent1.setEvaluationMethod(new EvaluationMethod(CrossValidation.class.getName()));
+		comAgent1.setEvaluationMethod(new EvaluationMethod(
+				CrossValidation.class.getName()));
 		comAgent1.setEvaluationMethod(evaluationMethod);
 		comAgent1.setModel(null);
 
-        //Labeled data labeled by our CA are the new datasource
+		// Labeled data labeled by our CA are the new datasource
 		DataSourceDescription computingDataSource1 = new DataSourceDescription();
 		computingDataSource1.setOutputType("Data");
 		computingDataSource1.setDataProvider(comAgent1);
@@ -60,35 +58,35 @@ public final class Input07 {
 		comAgent2.addOption(optionM);
 		comAgent2.setTrainingData(computingDataSource1);
 		comAgent2.setTestingData(computingDataSource1);
-		comAgent2.setEvaluationMethod(new EvaluationMethod(CrossValidation.class.getName()));
+		comAgent2.setEvaluationMethod(new EvaluationMethod(
+				CrossValidation.class.getName()));
 		comAgent2.setEvaluationMethod(evaluationMethod);
-	   	comAgent2.setModel(null);
-		
-        //Labeled data labeled by our CA are the new datasource
+		comAgent2.setModel(null);
+
+		// Labeled data labeled by our CA are the new datasource
 		DataSourceDescription computingDataSource2 = new DataSourceDescription();
 		computingDataSource2.setOutputType("Data");
 		computingDataSource2.setDataProvider(comAgent2);
-		
-        //Save labeled data
-        FileDataSaver saver = new FileDataSaver();
-        saver.setDataSource(computingDataSource2);
-        
-        ComputationDescription comDescription = new ComputationDescription();
-        comDescription.addRootElement(saver);
 
-        return comDescription;
+		// Save labeled data
+		FileDataSaver saver = new FileDataSaver();
+		saver.setDataSource(computingDataSource2);
+
+		ComputationDescription comDescription = new ComputationDescription();
+		comDescription.addRootElement(saver);
+
+		return comDescription;
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		System.out.println("Exporting Ontology input07 to Klara's input XML configuration file.");
 
 		ComputationDescription comDescription = createDescription();
-		
+
 		String fileName = CoreConfiguration.INPUTS_KLARA_PATH + "input07"
-				+ System.getProperty("file.separator")
-				+ "input.xml";
+				+ System.getProperty("file.separator") + "input.xml";
 
 		comDescription.exportXML(fileName);
-    }
+	}
 }
