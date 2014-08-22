ac9779863cf8aa2890535d0622479549c478f9ba
krajj7 <krajj7@users.noreply.github.com>
2014-08-22 18:35:18 +0200

    shared static method for direct mailing

--- src/org/pikater/core/agents/system/Agent_Mailing.java
+++ src/org/pikater/core/agents/system/Agent_Mailing.java
@@ -1,25 +1,26 @@
 package org.pikater.core.agents.system;
 
-import jade.domain.FIPAAgentManagement.NotUnderstoodException;
-import jade.domain.FIPAAgentManagement.RefuseException;
-import jade.lang.acl.ACLMessage;
-import jade.lang.acl.MessageTemplate;
-import jade.proto.AchieveREResponder;
 import jade.content.Concept;
 import jade.content.lang.Codec.CodecException;
 import jade.content.onto.Ontology;
 import jade.content.onto.OntologyException;
 import jade.content.onto.basic.Action;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREResponder;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.mail.*;
-import javax.mail.internet.*;
+import javax.mail.MessagingException;
 
 import org.pikater.core.AgentNames;
 import org.pikater.core.agents.PikaterAgent;
 import org.pikater.core.ontology.MailingOntology;
 import org.pikater.core.ontology.subtrees.mailing.SendEmail;
+import org.pikater.shared.utilities.mailing.Mailing;
 
 /** Agent, ktery resi komunikaci s mistnim SMTP serverem, tj. odesilani ruznych e-mailu i jejich tvorbu. */
 public class Agent_Mailing extends PikaterAgent {
@@ -29,10 +30,6 @@ public class Agent_Mailing extends PikaterAgent {
 	public static enum EmailType {
 		TEST, RESULT
 	};
-
-	/** Adresa, ktera bude uvedena ve FROM u mailu, ktere agent posle
-	 * TODO: nekam do konfigurace? */
-	private static final String SENDER_EMAIL = "pikater@noreply.cz";
 
 	@Override
 	public List<Ontology> getOntologies() {
@@ -81,7 +78,8 @@ public class Agent_Mailing extends PikaterAgent {
 		try {
 			switch (type) {
 			case TEST:
-				sendEmail(to, "Test message", "Example message from pikater MailAgent");
+				Mailing.sendEmail(to, "Test message", "Example message from pikater MailAgent");
+				log("sent e-mail to " + to);
 				break;
 			case RESULT:
 				//log("would send mail with results to "+to+", best error rate: "+mailAction.getResult());
@@ -90,7 +88,8 @@ public class Agent_Mailing extends PikaterAgent {
 					body += "\n\nThe best error rate achieved was " + String.format("%.2f %%", mailAction.getResult() * 100);
 				}
 				String subj = "Pikater batch finished (" + mailAction.getBatch_id() + ")";
-				sendEmail(to, subj, body);
+				Mailing.sendEmail(to, subj, body);
+				log("sent e-mail to " + to);
 				break;
 			default:
 				throw new UnsupportedOperationException();
@@ -117,26 +116,4 @@ public class Agent_Mailing extends PikaterAgent {
 			throw new NotUnderstoodException("Unknown e-mail type " + mailAction.getEmail_type());
 		}
 	}
-
-	/** Odesle plaintext e-mail dle parametru pomoci lokalniho SMTP serveru, nebo vyhodi vyjimku. */
-	private void sendEmail(String to, String subject, String body) throws MessagingException {
-		Properties properties = new Properties();
-		// predpokladam funkcni SMTP server na localhostu
-		properties.setProperty("mail.smtp.host", "localhost");
-
-		MimeMessage message = new MimeMessage(Session.getDefaultInstance(properties));
-		// od koho
-		message.setFrom(new InternetAddress(SENDER_EMAIL));
-		// komu
-		message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-		// predmet
-		message.setSubject(subject);
-		// telo
-		message.setText(body);
-
-		Transport.send(message);
-
-		log("sent e-mail to " + to);
-	}
-
 }
