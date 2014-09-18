902ba5de35c8225b3e6cbc787909f4bd7bc9e906
Jiří Smolík <kukurka@gmail.com>
2014-09-18 13:47:14 +0200

    Updating web logger to reflect recent abstraction

---
+++ src/org/pikater/shared/logging/web/PikaterWebLogger.java
@@ -0,0 +1,32 @@
+package org.pikater.shared.logging.web;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.pikater.shared.logging.GeneralPikaterLogger;
+import org.pikater.shared.logging.IPikaterLogger;
+
+public class PikaterWebLogger extends GeneralPikaterLogger
+{
+	private static final IPikaterLogger innerLogger = createPikaterLogger(Logger.getLogger("log4j"));
+	
+	public static IPikaterLogger getLogger()
+	{
+		return innerLogger;
+	}
+	
+	public static void logThrowable(String message, Throwable t)
+	{
+		getLogger().logThrowable(message, t);
+	}
+
+	public static void log(Level logLevel, String message)
+	{
+		getLogger().log(logLevel, message);
+	}
+	
+	public static void log(Level logLevel, String source, String message)
+	{
+		getLogger().log(logLevel, source, message);
+    }
+}
