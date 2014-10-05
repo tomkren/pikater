7950c4d6f17560447e1f8f2066235ddf500fcc2a
Peter <siposp@gmail.com>
2014-10-05 22:17:51 +0200

    PostgreSQL-specific package javadocs

---
+++ src/org/pikater/shared/database/postgre/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains classes implementing features, that are specific PostgreSQL database.
+ * <p>
+ * If the application is being ported to another database management system these classes should
+ * be revised. However, using Java Persistence API for majority of database operations, the amount of these
+ * modifications should me minimal.
+ * 
+ * @see org.pikater.shared.database.jpa
+ * @see org.pikater.shared.database.postgre.largeobject
+ */
+package org.pikater.shared.database.postgre;
