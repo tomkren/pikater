f598a43ce8491bd665986c3caf67447bad8926a2
Peter <siposp@gmail.com>
2014-10-08 23:50:36 +0200

    JPA status javadocs

--- src/org/pikater/shared/database/jpa/status/JPADatasetSource.java
+++ src/org/pikater/shared/database/jpa/status/JPADatasetSource.java
@@ -1,5 +1,11 @@
 package org.pikater.shared.database.jpa.status;
 
+/**
+ * Enum used to define the source of a dataset stored in the database. This status is needed
+ * for the reason that datasets can be uploaded by a user or created by an experiment. In several
+ * cases it's useful to distinguish between the sources.
+ *
+ */
 public enum JPADatasetSource {
 	EXPERIMENT,
 	USER_UPLOAD
