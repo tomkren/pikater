2d707e66fbcf4b14ed3a0ed0235656872d4fb02d
Peter <siposp@gmail.com>
2014-10-08 20:26:57 +0200

    Native DB refactoring and javadoc
    - connector javadoc
    - removing obsolete classes

--- src/org/pikater/shared/database/connection/IConnectionProvider.java
+++ src/org/pikater/shared/database/connection/IConnectionProvider.java
@@ -9,7 +9,22 @@ import java.sql.SQLException;
  * Time: 19:48
  */
 public interface IConnectionProvider {
+	/**
+	 * Creates a new connection using the data set in the constructor.
+	 * @return the {@link Connection} object
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
     public Connection getConnection() throws ClassNotFoundException, SQLException;
+    /**
+     * Returns the connection information for the current connection. In most cases this is
+     * the URL of the connection.
+     * @return {@link String} describing connection information
+     */
     public String getConnectionInfo();
+    /**
+     * Returns the name of schema used by the connection. 
+     * @return {@link String} containing schema name
+     */
     public String getSchema();
 }
--- src/org/pikater/shared/database/connection/PostgreSQLConnectionProvider.java
+++ src/org/pikater/shared/database/connection/PostgreSQLConnectionProvider.java
@@ -4,12 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Class {@link PostgreSQLConnectionProvider} is used to create a native database connection
+ * to PostgreSQL database management system.
+ */
 public class PostgreSQLConnectionProvider implements IConnectionProvider
 {
     private final String url;
     private final String userName;
     private final String password;
 
+    /**
+     * Constructor for setting the data needed to establish a new connection
+     * @param url of the database management system
+     * @param username to be used for the connection
+     * @param password to be used for the connection
+     */
     public PostgreSQLConnectionProvider(String url, String username, String password)
     {
         this.url = url;
