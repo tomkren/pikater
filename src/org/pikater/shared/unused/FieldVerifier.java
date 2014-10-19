0c8d891945cd294d10972dd27357760da51d09cc
Jiří Smolík <kukurka@gmail.com>
2014-10-19 19:01:47 +0200

    Removing code quality issues

--- src/org/pikater/shared/unused/FieldVerifier.java
+++ src/org/pikater/shared/unused/FieldVerifier.java
@@ -6,16 +6,13 @@ public class FieldVerifier {
 	}
 
 	public static boolean isValidEmail(String email) {
-		boolean result = false;
 		if (!isStringNullOrEmpty(email)) {
 			String[] splitted = email.split("@");
-			if (splitted.length == 2) {
-				// max email length: 64@255
-				if ((splitted[0].length() <= 64) && (splitted[1].length() <= 255) && splitted[1].contains(".")) {
-					result = true;
-				}
+			// max email length: 64@255
+			if ((splitted.length == 2) && (splitted[0].length() <= 64) && (splitted[1].length() <= 255) && splitted[1].contains(".")) {
+				return true;
 			}
 		}
-		return result;
+		return false;
 	}
 }
