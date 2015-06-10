8b40d5ae08591be1a9a72091aa63f7cd7b30f608
Klara <ustav@seznam.cz>
2015-06-10 15:56:02 +0200

    4 inputs and outputs for PCA added to Input_Tom1.

--- test/org/pikater/core/experiments/Input_Tom1.java
+++ test/org/pikater/core/experiments/Input_Tom1.java
@@ -1,5 +1,6 @@
 package org.pikater.core.experiments;
 
+import org.pikater.core.CoreConstant;
 import org.pikater.core.agents.experiment.computing.Agent_WekaRBFNetworkCA;
 import org.pikater.core.agents.experiment.dataprocessing.Agent_PCA;
 import org.pikater.core.agents.experiment.dataprocessing.errorcomputing.Agent_Accuracy;
@@ -20,9 +21,21 @@ public final class Input_Tom1 implements ITestExperiment {
 		fileDataProvider.setFileURI("weather.arff");
 
 		// Specify a datasource
-		DataSourceDescription fileDataSource = new DataSourceDescription();
-		fileDataSource.setDataProvider(fileDataProvider);
+		DataSourceDescription fileDataSource1 = new DataSourceDescription();
+		fileDataSource1.setDataProvider(fileDataProvider);
+		fileDataSource1.setInputType(CoreConstant.SlotContent.TRAINING_DATA.getSlotName());
 
+		DataSourceDescription fileDataSource2 = new DataSourceDescription();
+		fileDataSource2.setDataProvider(fileDataProvider);
+		fileDataSource2.setInputType(CoreConstant.SlotContent.TESTING_DATA.getSlotName());
+
+		DataSourceDescription fileDataSource3 = new DataSourceDescription();
+		fileDataSource3.setDataProvider(fileDataProvider);
+		fileDataSource3.setInputType(CoreConstant.SlotContent.VALIDATION_DATA.getSlotName());
+
+		DataSourceDescription fileDataSource4 = new DataSourceDescription();
+		fileDataSource4.setDataProvider(fileDataProvider);
+		fileDataSource4.setInputType(CoreConstant.SlotContent.DATA_TO_LABEL.getSlotName());
 
 		// PreProcessing
 
@@ -31,22 +44,38 @@ public final class Input_Tom1 implements ITestExperiment {
 		DataProcessing pca = new DataProcessing();
 		pca.setAgentType(Agent_PCA.class.getName());
 		pca.addOption(new NewOption("M",3)); // Maximum number of PC attributes to retain (-1 = include all, default: -1)
-		pca.addDataSources(fileDataSource);
+
+		pca.addDataSources(fileDataSource1);
+		pca.addDataSources(fileDataSource2);
+		pca.addDataSources(fileDataSource3);
+		pca.addDataSources(fileDataSource4);
 
 		// Data processed by pca are the new datasource
-		DataSourceDescription dataSourcePCA = new DataSourceDescription();
-		dataSourcePCA.setOutputType("Data");
-		dataSourcePCA.setDataProvider(pca);
+		DataSourceDescription dataSourcePCA1 = new DataSourceDescription();
+		dataSourcePCA1.setOutputType(CoreConstant.SlotContent.TRAINING_DATA.getSlotName());
+		dataSourcePCA1.setDataProvider(pca);
+
+		DataSourceDescription dataSourcePCA2 = new DataSourceDescription();
+		dataSourcePCA2.setOutputType(CoreConstant.SlotContent.TESTING_DATA.getSlotName());
+		dataSourcePCA2.setDataProvider(pca);
+
+		DataSourceDescription dataSourcePCA3 = new DataSourceDescription();
+		dataSourcePCA3.setOutputType(CoreConstant.SlotContent.VALIDATION_DATA.getSlotName());
+		dataSourcePCA3.setDataProvider(pca);
+
+		DataSourceDescription dataSourcePCA4 = new DataSourceDescription();
+		dataSourcePCA4.setOutputType(CoreConstant.SlotContent.DATA_TO_LABEL.getSlotName());
+		dataSourcePCA4.setDataProvider(pca);
 
 
 		// Create new computing agent, add options and datasource that we have
 		// created above
 		ComputingAgent rbf = new ComputingAgent();
 		rbf.setAgentType(Agent_WekaRBFNetworkCA.class.getName());
-		rbf.setTrainingData(dataSourcePCA);
-		rbf.setTestingData(dataSourcePCA);
-		rbf.setValidationData(dataSourcePCA);
-		rbf.setDataToLabel(dataSourcePCA);
+		rbf.setTrainingData(dataSourcePCA1);
+		rbf.setTestingData(dataSourcePCA2);
+		rbf.setValidationData(dataSourcePCA3);
+		rbf.setDataToLabel(dataSourcePCA4);
 
 		rbf.setEvaluationMethod(new EvaluationMethod(Standard.class.getName()));
 
@@ -55,6 +84,7 @@ public final class Input_Tom1 implements ITestExperiment {
 		labeledDataSourceRBF.setOutputType("Data");
 		labeledDataSourceRBF.setDataProvider(rbf);
 
+		/*
 		// compute error
 		DataProcessing err = new DataProcessing();
 		err.setAgentType(Agent_Accuracy.class.getName());
@@ -65,10 +95,11 @@ public final class Input_Tom1 implements ITestExperiment {
 		DataSourceDescription errDataSource = new DataSourceDescription();
 		errDataSource.setOutputType("Error");
 		errDataSource.setDataProvider(err);
+		*/
 
 		// Save labeled data
 		FileDataSaver saver = new FileDataSaver();
-		saver.setDataSource(errDataSource);
+		saver.setDataSource(labeledDataSourceRBF);
 
 		ComputationDescription comDescription = new ComputationDescription();
 		comDescription.addRootElement(saver);
